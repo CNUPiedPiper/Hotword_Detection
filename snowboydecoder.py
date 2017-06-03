0a6e41c20b422034b9ee673140b59be4860f7370
Yoon-jae <yoonjae.cho92@gmail.com>
2017-06-03 14:52:09 +0900

    Edit call start() again after terminate()

--- snowboydecoder.py
+++ snowboydecoder.py
@@ -74,11 +74,6 @@ class HotwordDetector(object):
                  sensitivity=[],
                  audio_gain=1):
 
-        def audio_callback(in_data, frame_count, time_info, status):
-            self.ring_buffer.extend(in_data)
-            play_data = chr(0) * len(in_data)
-            return play_data, pyaudio.paContinue
-
         tm = type(decoder_model)
         ts = type(sensitivity)
         if tm is not list:
@@ -104,26 +99,6 @@ class HotwordDetector(object):
 
         self.ring_buffer = RingBuffer(
             self.detector.NumChannels() * self.detector.SampleRate() * 5)
-        self.audio = pyaudio.PyAudio()
-
-        ''' default
-        self.stream_in = self.audio.open(
-            input=True, output=False,
-            format=self.audio.get_format_from_width(
-                self.detector.BitsPerSample() / 8),
-            channels=self.detector.NumChannels(),
-            rate=self.detector.SampleRate(),
-            frames_per_buffer=2048,
-            stream_callback=audio_callback)
-        '''
-
-        self.stream_in = self.audio.open(
-            input=True, output=False,
-            format=pyaudio.paInt16,
-            channels=self.detector.NumChannels(),
-            rate=44100,
-            frames_per_buffer=8192,
-            stream_callback=audio_callback)
 
     def start(self, detected_callback=play_audio_file,
               interrupt_check=lambda: False,
@@ -144,6 +119,21 @@ class HotwordDetector(object):
         :param float sleep_time: how much time in second every loop waits.
         :return: None
         """
+
+        def audio_callback(in_data, frame_count, time_info, status):
+            self.ring_buffer.extend(in_data)
+            play_data = chr(0) * len(in_data)
+            return play_data, pyaudio.paContinue
+
+        self.audio = pyaudio.PyAudio()
+        self.stream_in = self.audio.open(
+            input=True, output=False,
+            format=pyaudio.paInt16,
+            channels=self.detector.NumChannels(),
+            rate=44100,
+            frames_per_buffer=8192,
+            stream_callback=audio_callback)
+
         if interrupt_check():
             logger.debug("detect voice return")
             return
